5f4296c50494afc9682b683e56107683e9662240
shenikar <shenikar@student.21-school.ru>
2025-09-18 23:07:20 +0300

    added ways to stop by context and channel and Goxit

--- 1.6/main.go
+++ 1.6/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"runtime"
 	"time"
 )
 
@@ -13,6 +15,15 @@ func main() {
 	fmt.Println("\n=== Пример 2: Выход по таймеру ===")
 	exitByTimer()
 	time.Sleep(2 * time.Second)
+
+	fmt.Println("\n=== Пример 3: Выход по контексту ===")
+	exitByContext()
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("\n=== Пример 4: Выход по каналу ===")
+	exitByChannel()
+	time.Sleep(2 * time.Second)
+
 }
 
 // 1. Выход по условию
@@ -48,4 +59,58 @@ func exitByTimer() {
 		}
 	}()
 	time.Sleep(2 * time.Second)
+}
+
+// 3. Выход по контексту
+func exitByContext() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		for {
+			select {
+			case <-ctx.Done(): // получаем сигнал остановки
+				fmt.Println("Горутина завершается по контексту")
+				return
+			default:
+				fmt.Println("Горутина работает")
+				time.Sleep(300 * time.Millisecond)
+			}
+		}
+	}()
+
+	time.Sleep(1 * time.Second)
+	cancel() // вызываем отмену контекста
+	time.Sleep(1 * time.Second)
+}
+
+// 4. Выход по каналу
+func exitByChannel() {
+	stop := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-stop: // получаем сигнал остановки
+				fmt.Println("Горутина завершается по каналу")
+				return
+			default:
+				fmt.Println("Горутина работает")
+				time.Sleep(300 * time.Millisecond)
+			}
+		}
+	}()
+
+	time.Sleep(1 * time.Second)
+	close(stop)
+	time.Sleep(1 * time.Second)
+}
+
+// 5. Выход по runtime.Goexit
+// Этот метод завершает только текущую горутину, не влияя на другие.
+func exitByGoexit() {
+	go func() {
+		fmt.Println("Горутина завершает себя через Goexit")
+		runtime.Goexit()
+		fmt.Println("Этот код не будет выполнен")
+
+	}()
+	time.Sleep(1 * time.Second)
 }
