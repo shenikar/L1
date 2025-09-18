6ab3ca5c64829aeb2dcb6562182b2e015786c31b
shenikar <shenikar@student.21-school.ru>
2025-09-18 22:52:15 +0300

    added ways to stop by timer

--- 1.6/main.go
+++ 1.6/main.go
@@ -9,6 +9,10 @@ func main() {
 	fmt.Println("=== Пример 1: Выход по условию ===")
 	exitByCondition()
 	time.Sleep(2 * time.Second)
+
+	fmt.Println("\n=== Пример 2: Выход по таймеру ===")
+	exitByTimer()
+	time.Sleep(2 * time.Second)
 }
 
 // 1. Выход по условию
@@ -26,4 +30,22 @@ func exitByCondition() {
 		}
 	}()
 	time.Sleep(1 * time.Second)
+}
+
+// 2. Выход по таймеру
+func exitByTimer() {
+	stop := time.After(2 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-stop: // получаем сигнал остановки
+				fmt.Println("Горутина завершается по таймеру")
+				return
+			default:
+				fmt.Println("Горутина работает")
+				time.Sleep(300 * time.Millisecond)
+			}
+		}
+	}()
+	time.Sleep(2 * time.Second)
 }
